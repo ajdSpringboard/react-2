062596e5feefdeca6835c3802c71acf1d8c3eca0
Alec Dieken <ajdieken@live.com>
2023-03-28 04:40:15 -0700

    Update unroll.js

--- unroll/unroll.js
+++ unroll/unroll.js
@@ -6,27 +6,24 @@ function unroll(square) {
     let colEnd = square.length - 1;
   
     while (rowStart <= rowEnd && colStart <= colEnd) {
-      // Traverse right
+
       for (let i = colStart; i <= colEnd; i++) {
         result.push(square[rowStart][i]);
       }
       rowStart++;
   
-      // Traverse down
       for (let i = rowStart; i <= rowEnd; i++) {
         result.push(square[i][colEnd]);
       }
       colEnd--;
   
-      // Traverse left
       if (rowStart <= rowEnd) {
         for (let i = colEnd; i >= colStart; i--) {
           result.push(square[rowEnd][i]);
         }
         rowEnd--;
       }
-  
-      // Traverse up
+
       if (colStart <= colEnd) {
         for (let i = rowEnd; i >= rowStart; i--) {
           result.push(square[i][colStart]);
